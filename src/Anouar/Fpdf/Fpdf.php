ae2064abbe5b79827a336368318cb456e2fe0524
Diogo Besson <dfernandes@camarasbc.sp.gov.br>
2023-02-17 16:11:26 -0300

    update Fpdf.php to newer versions of PHP compat
    verification of existance of magic_quotes

--- src/Anouar/Fpdf/Fpdf.php
+++ src/Anouar/Fpdf/Fpdf.php
@@ -1051,7 +1051,7 @@ function _dochecks()
 	if(ini_get('mbstring.func_overload') & 2)
 		$this->Error('mbstring overloading must be disabled');
 	// Ensure runtime magic quotes are disabled
-	if(get_magic_quotes_runtime())
+	if(function_exists(get_magic_quotes_runtime()))
 		@set_magic_quotes_runtime(0);
 }
 
